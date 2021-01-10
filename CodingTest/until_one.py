75c5eb856b1d93dbceba92d5457e169c9c99a71d
leesh96 <suho2718@gmail.com>
2021-01-11 08:01:55 +0900

    코테 연습

--- CodingTest/until_one.py
+++ CodingTest/until_one.py
@@ -12,14 +12,6 @@ while N != 1:
     else:
         count += (N % K)
         N -= (N % K)
-# while True:
-#     target = (N // K) * K
-#     count += (N - target)
-#     N = target
-#     if N < K: break
-#     count += 1
-#     N //= K
-# count += (N - 1)
 end_time = time.time()
 
 print(end_time - start_time)
